ce1b2e30ed0da449d687fe575a53d812a29e9f4d
Sparsh <sparsh.chadha46@gmail.com>
2023-04-04 19:12:21 +0530

    new commit

---
+++ src/Graphs/BipartiteGraph.java
@@ -0,0 +1,36 @@
+package Graphs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BipartiteGraph {
+    class Solution {
+        public boolean isBipartite(int[][] graph) {
+            int n = graph.length;
+            int[] color = new int[n];
+
+            for(int i = 0; i < n; i++){
+                if(color[i] == 1 || color[i] == -1) continue;
+
+                Queue<Integer> queue = new LinkedList<>();
+                queue.offer(i);
+                color[i] = 1;
+
+                while(!queue.isEmpty()){
+                    int curr = queue.poll();
+
+                    for(int next : graph[curr]){
+                        if(color[next] == 0){
+                            color[next] = -color[curr];
+                            queue.offer(next);
+                        }
+
+                        if(color[next] == color[curr]) return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
